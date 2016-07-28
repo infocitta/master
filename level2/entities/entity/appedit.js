53a5fc63d172fd19f3949d7de2f3dae48124393a
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-28 16:07:35 +0200

    editor in corso

---
+++ level2/entities/entity/appedit.js
@@ -0,0 +1,30 @@
+﻿// Code goes here
+
+var myApp = angular.module('myApp', []);
+
+function MyController($scope, $http) {
+
+  $scope.currentPage = 1;
+  $scope.pageSize = 20;
+  $scope.weekdayname = ['','Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato','Domenica'];
+  $scope.today= new Date().toJSON();
+ $http.get("https://raw.githubusercontent.com/infocitta/infocitta.github.io/master/desio/asilo_nido/asilo_nido_comunale_via_diaz/entity.json")
+    .then(function(response) {
+        $scope.entity = response.data;
+    });
+
+
+	
+  $scope.pageChangeHandler = function(num) {
+      
+  };
+}
+
+function OtherController($scope) {
+  $scope.pageChangeHandler = function(num) {
+  
+  };
+}
+
+myApp.controller('MyController', MyController);
+myApp.controller('OtherController', OtherController);
