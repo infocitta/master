0c0fd30028452bd6adaa6393f3be37355eef4bef
Paolo Fisco <paolo.fisco@gmail.com>
2016-07-30 00:18:23 +0200

    .

--- level2/entities/entity/appedit.js
+++ level2/entities/entity/appedit.js
@@ -8,18 +8,54 @@ function MyController($scope, $http) {
   $scope.pageSize = 20;
   $scope.weekdayname = ['','Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato','Domenica'];
   $scope.today= new Date().toJSON();
+  
+
+  $scope.contatto = [
+      {id: '0', name: 'Telefono'},
+      {id: '1', name: 'Email'},
+      {id: '2', name: 'Link'}
+    ];	
+  
+  
  $http.get("https://raw.githubusercontent.com/infocitta/infocitta.github.io/master/desio/asilo_nido/asilo_nido_comunale_via_diaz/entity.json")
     .then(function(response) {
         $scope.entity = response.data;
     });
-
+	
+	
+//Indirizzi
 $scope.addAddr=function () {
-	$scope.entity.Contact.push({Value: ''});
+	$scope.entity.Adresses.push({ Title: 'Indirizzo', Value: '', Latitude: '', Longitude: '', Note: '' });
 }
 	
-$scope.removeContact = function(index) {
+$scope.removeAddr = function(index) {
 	if (confirm('Corfema eliminazione?')) {
-		$scope.entity.Contact.splice(index, 1);     
+		$scope.entity.Adresses.splice(index, 1);     
+	} 	 
+}
+
+
+//Contatti
+
+$scope.addCt=function () {
+	$scope.entity.Contacts.push({Title:'Contatto',Value:'',Note: '',Type:0 });
+}
+	
+$scope.removeCt = function(index) {
+	if (confirm('Corfema eliminazione?')) {
+		$scope.entity.Contacts.splice(index, 1);     
+	} 	 
+}
+
+//Note
+
+$scope.addNt=function () {
+	$scope.entity.Notes.push({Title: 'Note',Value: ''    });
+}
+	
+$scope.removeNt = function(index) {
+	if (confirm('Corfema eliminazione?')) {
+		$scope.entity.Notes.splice(index, 1);     
 	} 	 
 }
 	
